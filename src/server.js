38b97f59cc2085b5cb26819ed91b482dec05840c
JisinKeo <tntjd9514@naver.com>
2022-11-16 01:24:07 +0900

    Chat Completed

--- src/server.js
+++ src/server.js
@@ -11,18 +11,20 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
 const server = http.createServer(app);
-
 const wss = new WebSocket.Server({ server });
+function onSocketClose() {
+  console.log("Disconnected from the Browser ❌");
+}
+const sockets = [];
 
 wss.on("connection", (socket) => {
-  console.log("Connected to Browser");
-  socket.on("close", () => console.log("Disconnected from the Browser"));
+  sockets.push(socket);
+  console.log("Connected to Browser ✅");
+  socket.on("close", onSocketClose);
   socket.on("message", (message) => {
-    console.log(message.toString('utf-8'));
+    sockets.forEach((aSocket) => aSocket.send(message));
   });
-  socket.send("hello");
-})
+});
 
 server.listen(3000, handleListen);
